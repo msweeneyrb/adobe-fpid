b7cd12ff126a73ef4123cadd1b914b3c03a5aa4e
rnb_msweeney <msweeney@roomandboard.com>
2024-09-17 10:09:36 -0500

    Adobe FPID worker

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,39 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+
+import { v4 as uuidv4 } from 'uuid';
+import { parse } from 'cookie';
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+	async fetch(request): Promise<Response> {
+		const url = new URL(request.url);
+        const path = url.pathname;
+        const extension = path.split('.').pop() || null;
+		const acceptHeader = request.headers.get('Accept') || '';
+
+        // Skip processing non-page or api requests
+        if (!extension || path.startsWith('/api') || !acceptHeader.includes('text/html')) {
+            return fetch(request);
+        }
+
+		const COOKIE_NAME = "FPID";
+		const cookie = parse(request.headers.get("Cookie") || "");
+		const cookieOptions = 'Secure; SameSite=Lax; Path=/; Max-Age=31536000;';
+		let cookieValue;
+
+		if (cookie[COOKIE_NAME] != null) {
+			cookieValue = cookie[COOKIE_NAME];
+		} else {
+			cookieValue = uuidv4();
+		}
+
+		const originalResponse = await fetch(request);
+
+		const response = new Response(originalResponse.body, originalResponse);
+
+		response.headers.append('Set-Cookie', `${COOKIE_NAME}=${cookieValue}; ${cookieOptions}`);
+
+		return response;
+	}
+}  satisfies ExportedHandler<Env>;;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,25 +1,40 @@
 // test/index.spec.ts
 import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
+import { getCookie } from '../src/index';
 import worker from '../src/index';
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
+describe('Adobe FPID', () => {
+	it('should have the FPID cookie in the response header', async () => {
 		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		const cookieHeader = response.headers.get('Set-Cookie');
+		expect(cookieHeader).toContain('FPID=');
+	  });
+});
+
+describe('getCookie', () => {
+	it('should return the cookie value if it exists', () => {
+	  const cookies = 'FPID=existing-cookie;';
+	  const name = 'FPID';
+
+	  const result = getCookie(cookies, name);
+
+	  expect(result).toBe('existing-cookie');
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+	it('should return null if the cookie does not exist', () => {
+	  const cookies = '';
+	  const name = 'FPID';
+
+	  const result = getCookie(cookies, name);
+
+	  expect(result).toBe(null);
 	});
-});
+  });
